9f951eae6161266e3094447751f408227e4fbf10
Vitaliy Sulimenko <prevetal@gmail.com>
2024-10-23 14:19:23 +0400

    Add modal

--- js/scripts.js
+++ js/scripts.js
@@ -184,6 +184,29 @@ document.addEventListener('DOMContentLoaded', function () {
 		MODAL: 'Вы можете закрыть это модальное окно нажав клавишу ESC'
 	}
 
+	Fancybox.defaults.tpl = {
+		closeButton: '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg><use xlink:href="images/sprite.svg#ic_close2"></use></svg></button>',
+
+		main: `<div class="fancybox__container" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1">
+			<div class="fancybox__backdrop"></div>
+			<div class="fancybox__carousel"></div>
+			<div class="fancybox__footer"></div>
+		</div>`,
+	}
+
+
+	// Modals
+	$('.modal_btn').click(function(e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: document.getElementById(e.target.getAttribute('data-modal')),
+			type: 'inline'
+		}])
+	})
+
 
 	// Zoom images
 	Fancybox.bind('.fancy_img', {
